07985d4ae39ac490b4eda0a7e79346ecd54e76ae
Allen Thich <allenthich@gmail.com>
2020-07-12 23:19:31 -0700

    Adjust header navigation styles and redirect logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
+/**
+ * Initialize foundation scripts
+ */
+const initFoundation = () => {
+  $(document).foundation()
+}
 
-  $(document).foundation();
+/**
+ * Prevents redirect from occurring when the active header is clicked
+ */
+const preventSameHeaderRedirect = () => {
+  $(document).on('click', '.menu a', function (e) {
+    if (e.currentTarget != this) return;
 
+    // Check if same page
+    if (e.currentTarget.pathname === window.location.pathname) {
+      e.preventDefault()
+    }
+  })
+}
+
+/**
+ * Main execution
+ */
+$(document).ready(() => {
+  initFoundation()
+  preventSameHeaderRedirect()
 });
